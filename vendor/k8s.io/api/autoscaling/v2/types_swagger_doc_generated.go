8bdba375d8036e4eb51bf5d52afc30a1017bac44
Arjun <agadiyar@nvidia.com>
2026-05-04 17:01:28 +0000

    Updated spelling

--- vendor/k8s.io/api/autoscaling/v2/types_swagger_doc_generated.go
+++ vendor/k8s.io/api/autoscaling/v2/types_swagger_doc_generated.go
@@ -117,7 +117,7 @@ func (HorizontalPodAutoscaler) SwaggerDoc() map[string]string {
 var map_HorizontalPodAutoscalerBehavior = map[string]string{
 	"":          "HorizontalPodAutoscalerBehavior configures the scaling behavior of the target in both Up and Down directions (scaleUp and scaleDown fields respectively).",
 	"scaleUp":   "scaleUp is scaling policy for scaling Up. If not set, the default value is the higher of:\n  * increase no more than 4 pods per 60 seconds\n  * double the number of pods per 60 seconds\nNo stabilization is used.",
-	"scaleDown": "scaleDown is scaling policy for scaling Down. If not set, the default value is to allow to scale down to minReplicas pods, with a 300 second stabilization window (i.e., the highest recommendation for the last 300sec is used).",
+	"scaleDown": "scaleDown is scaling policy for scaling Down. If not set, the default value is to allow scaling down to minReplicas pods, with a 300 second stabilization window (i.e., the highest recommendation for the last 300sec is used).",
 }
 
 func (HorizontalPodAutoscalerBehavior) SwaggerDoc() map[string]string {
--- vendor/k8s.io/api/scheduling/v1alpha2/types_swagger_doc_generated.go
+++ vendor/k8s.io/api/scheduling/v1alpha2/types_swagger_doc_generated.go
@@ -164,7 +164,7 @@ func (TopologyConstraint) SwaggerDoc() map[string]string {
 }
 
 var map_TypedLocalObjectReference = map[string]string{
-	"":         "TypedLocalObjectReference allows to reference typed object inside the same namespace.",
+	"":         "TypedLocalObjectReference allows referencing a typed object inside the same namespace.",
 	"apiGroup": "APIGroup is the group for the resource being referenced. If APIGroup is empty, the specified Kind must be in the core API group. For any other third-party types, setting APIGroup is required. It must be a DNS subdomain.",
 	"kind":     "Kind is the type of resource being referenced. It must be a path segment name.",
 	"name":     "Name is the name of resource being referenced. It must be a path segment name.",
